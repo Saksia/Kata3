f2eb8904cdd73e58312f125b75f2bd9dfd20c906
Saksia <saskia.santana101@alu.ulpgc.es>
2016-01-10 16:46:47 +0000

    Update Kata3.java

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,20 +1,26 @@
+
 package kata3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Kata3 {
-    
+
     public static void main(String[] args) {
-        Histogram <String> histogram = new Histogram <>();
-        histogram.increment("gmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("yahoo.com");
+        Histogram<String> histo = new Histogram<>();
+        String pathName = "/Users/Indra/Desktop/email.txt";
+
+        try{
+            BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
+            String mail;
+            while((mail = fileIn.readLine()) != null){
+                if(!mail.contains("@")) continue;
+            }
+            histo.increment(mail.split("@")[1].toLowerCase());
+        } catch (Exception e) {
+            System.out.println("El fichero no existe");
+        }
         
-        HistogramDisplay histo = new HistogramDisplay(histogram);
-        histo.execute();
-        
-   
-        
+        HistogramDisplay histoDisplay = new HistogramDisplay(histo);
+        histoDisplay.execute();
     }
-    
-}
