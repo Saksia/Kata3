7b5e814b704af31de0d1baede3f1e9f13ce56569
Saksia <saskia.santana101@alu.ulpgc.es>
2016-01-10 16:49:14 +0000

    version1

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -1,15 +1,13 @@
 
 package kata3;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Kata3 {
-
     public static void main(String[] args) {
         Histogram<String> histo = new Histogram<>();
         String pathName = "/Users/Saskia/Desktop/email.txt";
-
+        
         try{
             BufferedReader fileIn = new BufferedReader(new FileReader(pathName));
             String mail;
